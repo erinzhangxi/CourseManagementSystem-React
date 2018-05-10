5155719e2203894cec41327fd50f47720b9943a1
erinzhangxi <erincheung1101@gmail.com>
2018-05-10 12:02:26 -0400

    add user-admin js and html

---
+++ src/main/java/com/example/webdev/model/Hello.java
@@ -0,0 +1,16 @@
+package com.example.webdev.model;
+import javax.persistence.*;
+
+
+@Entity
+public class Hello {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String message;
+	public int getId() 					{	return id;          }
+	public void setId(int id)				{	this.id = id;       }
+	public String getMessage()			{	return message;     }
+	public void setMessage(String msg)	{	this.message = msg; }
+
+}
