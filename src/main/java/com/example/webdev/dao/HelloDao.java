9a67fad7ac0debbbcd4d37dafd80a9e281cabb8f
erinzhangxi <erincheung1101@gmail.com>
2018-05-10 12:24:47 -0400

    modify git remote url

---
+++ src/main/java/com/example/webdev/dao/HelloDao.java
@@ -0,0 +1,19 @@
+//package com.example.webdev.dao;
+//
+//import java.util.List;
+//
+//import com.example.webdev.model.Hello;
+//
+//public class HelloDao {
+//	String findAllMessagesSql= "SELECT * FROM messages";
+//	
+//	public List<Hello> findAllMovies() {	
+//		List<Hello> messages = new ArrayList<Hello>();
+//		Connection 
+//		return null;
+//	}
+//	public Hello findById(String id) {	return null;}
+//	public void create(Hello hello) {}
+//	public void delete(String s) {}
+//	public void update(String s, Hello hello) {}
+//}
