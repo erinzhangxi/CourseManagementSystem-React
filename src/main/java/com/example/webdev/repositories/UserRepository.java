1d9b9588d04d35d1506113e97a29b3629b5f4e76
erinzhangxi <erincheung1101@gmail.com>
2018-05-12 14:39:27 -0400

    added user profile page

--- src/main/java/com/example/webdev/repositories/UserRepository.java
+++ src/main/java/com/example/webdev/repositories/UserRepository.java
@@ -1,10 +1,15 @@
 package com.example.webdev.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Query;
 import com.example.webdev.model.User;
 
-public interface UserRepository
-extends CrudRepository<User, Integer>{
 
+public interface UserRepository
+	extends CrudRepository<User, Integer>{
+	@Query("SELECT u FROM User u WHERE u.username=:username AND u.password=:password")
+	Iterable<User> findUserByCredentials(
+		@Param("username") String username, 
+		@Param("password") String password);
 }
---
+++ src/main/webapp/jquery/components/profile/profile.controller.client.js
@@ -0,0 +1,103 @@
+//IIFE
+(function () {
+
+    jQuery(main);
+
+    var $staticEmail;
+    var $firstName;
+    var $lastName;
+    var $updateBtn;
+    var userService = new UserServiceClient();
+
+    function init() {
+      $staticEmail = $("#staticEmail");
+      $firstName = $("#firstName");
+      $lastName = $("#lastName");
+      $updateBtn = $("#updateBtn")
+        .click(updateUser);
+      findUserById(12);
+    }
+
+    function updateUser() {
+      var user = {
+        firstName = $firstName.val();
+        lastName = $lastName.val();
+      };
+
+      userService.updateUser(12, user);
+    }
+
+
+
+
+
+
+
+    function main() {
+        tbody = $('tbody');
+        template = $('.template');
+        $('#createUser').click(createUser);
+
+        var promise = fetch('http://localhost:8080/api/user');
+        promise.then(function (response) {
+            return response.json();
+        }).then(renderUsers)
+    }
+
+    function createUser() {
+        console.log('createUser');
+
+        var username = $('#usernameFld').val();
+        var password = $('#passwordFld').val();
+        var firstName = $('#firstNameFld').val();
+        var lastName = $('#lastNameFld').val();
+
+        var user = {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName
+        };
+
+        fetch('http://localhost:8080/api/user', {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+
+        // update all users after create
+    }
+
+// this is now appending all users; will have redundant users
+    function renderUsers(users) {
+        for(var i=0; i<users.length; i++) {
+            var user = users[i];
+            var clone = template.clone();
+            clone.attr('id', user.id);
+            clone.find('.delete').click(deleteUser);
+            clone.find('.edit').click(editUser);
+            clone.find('.username')
+                .html(user.username);
+            tbody.append(clone);
+        }
+    }
+
+    function deleteUser(event) {
+      var deleteBtn = $(event.currentTarget);
+      var userId = deleteBtn
+        .parent()
+        .parent()
+        .attr('id');
+
+      userService
+        .deleteUser(userId)
+        .then(findAllUsers);
+    }
+
+    function editUser(event) {
+      console.log('edit user');
+      console.log(event);
+    }
+})();
