e55f4514a4464d7604a3f73fc129e8e331fab716
erinzhangxi <erincheung1101@gmail.com>
2018-05-20 01:15:48 -0400

    completed React lecture tutorials & updated course model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWorld from './hello'
-import CourseManager from './components/CourseManager'
+import CourseManager from './containers/CourseManager'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import Hello from './components/Hello'
+import Stateless from './components/Stateless'
+import ModuleListItem from './components/ModuleListItem'
+import ModuleList2 from './containers/ModuleList2'
+import App from './examples/App'
 
 ReactDOM.render(
-<CourseManager/>,
+    <div>
+    <App/>
+{/*<ModuleList2/>*/}
+{/*<Stateless message="this is a stateless component"/>*/}
+    </div>,
     document.getElementById('root')
 );
